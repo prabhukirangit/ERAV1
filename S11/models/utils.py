361ae9a0066a93c2c37a4175cc3bfbb71cb8c52e
prabhukirangit <33514187+prabhukirangit@users.noreply.github.com>
2023-08-01 13:30:23 +0530

    Delete utils.py

--- S11/models/utils.py
+++
@@ -1,8 +0,0 @@
-
-
-def get_lr(optimizer):
-    """"
-    for tracking how your learning rate is changing throughout training
-    """
-    for param_group in optimizer.param_groups:
-        return param_group['lr']
