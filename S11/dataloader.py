0f4a9688a94cca222901a3357fb6c3322aa94275
prabhukirangit <33514187+prabhukirangit@users.noreply.github.com>
2023-08-01 16:13:09 +0530

    Update dataloader.py

--- S11/dataloader.py
+++ S11/dataloader.py
@@ -1,1 +1,6 @@
+from torchvision import datasets,transforms
 
+class CustomCIFAR10Dataset(datasets.CIFAR10):
+  def __init__(self, root="~/data", train=True, download=True, transform=None):
+    self.data=super().__init__(root=root, train=train, download=download, transform=transform)
+    self.mean, self.std = ( 0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)
